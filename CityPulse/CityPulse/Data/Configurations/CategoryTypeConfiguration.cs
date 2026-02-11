34995c27aa03c290bd9d4aa601228d02829bb322
Elena Mitkova <elenamitkovaa77@gmail.com>
2026-02-11 21:46:18 +0200

    Categories and Cities Configurations

--- CityPulse/CityPulse/Data/Configurations/CategoryTypeConfiguration.cs
+++ CityPulse/CityPulse/Data/Configurations/CategoryTypeConfiguration.cs
@@ -1,6 +1,42 @@
-﻿namespace CityPulse.Data.Configurations
+﻿using CityPulse.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CityPulse.Data.Configurations
 {
-    public class CategoryTypeConfiguration
+    public class CategoryTypeConfiguration : IEntityTypeConfiguration<Category>
     {
+        private readonly List<Category> Categories = new List<Category>
+        {
+            new Category
+            {
+                Name = "Potholes / Road Damage"
+            },
+            new Category
+            {
+                Name = "Sidewalks / Pedestrian Zones"
+            },
+            new Category
+            {
+                Name = "Street Lighting"
+            },
+            new Category
+            {
+                Name = "Park Furniture"
+            },
+            new Category
+            {
+                Name = "Playgrounds / Sports Facilities"
+            },
+            new Category
+            {
+                Name = "Other"
+            }
+        };
+
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(Categories);
+        }
     }
 }
---
+++ CityPulse/CityPulse/Data/Configurations/CityTypeConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using CityPulse.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CityPulse.Data.Configurations
+{
+    public class CityTypeConfiguration : IEntityTypeConfiguration<City>
+    {
+        private readonly List<City> Cities = new List<City>
+        {
+            new City
+            {
+                Name = "Sofia"
+            },
+            new City
+            {
+                Name = "Plovdiv"
+            },
+            new City
+            {
+                Name = "Varna"
+            },
+            new City
+            {
+                Name = "Blagoevgrad"
+            },
+            new City
+            {
+                Name = "Burgas"
+            },
+            new City
+            {
+                Name = "Vidin"
+            },
+            new City
+            {
+                Name = "Pleven"
+            },
+            new City
+            {
+                Name = "Ruse"
+            }
+        };
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder.HasData(Cities);
+        }
+    }
+}
