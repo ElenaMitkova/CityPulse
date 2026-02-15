a6562e517294ca23a6fd7c6ad7311bd3faaf1f63
Elena Mitkova <elenamitkovaa77@gmail.com>
2026-02-15 22:21:39 +0200

    Cities districts index page

---
+++ CityPulse/CityPulse/ViewModels/LocationsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CityPulse.Models;
+
+namespace CityPulse.ViewModels
+{
+    public class LocationsViewModel
+    {
+        public IEnumerable<City> Cities { get; set; } = new HashSet<City>();
+        public IEnumerable<District> Districts { get; set; } = new HashSet<District>();
+    }
+}
