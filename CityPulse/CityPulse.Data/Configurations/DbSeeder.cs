68223eda2f9d773e0be2e282d82ec4907a1cc147
Elena Mitkova <elenamitkovaa77@gmail.com>
2026-04-05 14:10:13 +0300

    admin

---
+++ CityPulse/CityPulse.Data/Configurations/DbSeeder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DbSeeder
+{
+    public static async Task SeedRolesAndAdminAsync(IServiceProvider serviceProvider)
+    {
+        RoleManager<IdentityRole> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        UserManager<IdentityUser> userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+        string[] roleNames = { "Administrator", "User" };
+        foreach (var roleName in roleNames)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+        }
+
+        string adminEmail = "admin@citypulse.com";
+        IdentityUser? adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+        if (adminUser == null)
+        {
+            IdentityUser newAdmin = new IdentityUser
+            {
+                UserName = adminEmail,
+                Email = adminEmail,
+                EmailConfirmed = true
+            };
+
+            IdentityResult result = await userManager.CreateAsync(newAdmin, "Admin123!");
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(newAdmin, "Administrator");
+            }
+        }
+    }
+}
