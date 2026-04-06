d0b790028eb272bc93bd5e7799d4efdc0de49eac
Elena Mitkova <elenamitkovaa77@gmail.com>
2026-04-06 20:17:14 +0300

    comment interface

---
+++ CityPulse/CityPulse.Services/Common/ICommentsService.cs
@@ -0,0 +1,8 @@
+﻿namespace CityPulse.Services.Common
+{
+    public interface ICommentsService
+    {
+        Task AddCommentAsync(int reportId, string userId, string content);
+        Task<bool> DeleteCommentAsync(int commentId, string userId, bool isAdmin);
+    }
+}
